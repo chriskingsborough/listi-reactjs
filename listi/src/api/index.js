2a3a44c0b8ad607ec11e7d12db7433b685f54d9f
David Jarzombek <davidjarzombek@Davids-MacBook-Pro.local>
2020-10-12 14:44:25 -0400

    add api base

---
+++ listi/src/api/index.js
@@ -0,0 +1,7 @@
+// file for endpoints
+
+
+
+// login examples
+//  https://medium.com/javascript-in-plain-english/all-in-one-tutorial-on-how-to-create-the-authentication-part-of-your-react-app-2530e7435629
+
